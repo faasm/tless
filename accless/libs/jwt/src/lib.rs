3ff30e36b5c87556fa469e43ffbe72fffe12bbcc
Carlos Segarra <carlos@carlossegarra.com>
2025-04-24 17:15:58 +0000

    jwt: run fmt and clippy

--- accless/libs/jwt/src/lib.rs
+++ accless/libs/jwt/src/lib.rs
@@ -4,7 +4,10 @@ use rsa::sha2::Sha256;
 use rsa::signature::Verifier;
 use rsa::RsaPublicKey;
 use serde_json::Value;
-use std::{ffi::{c_char, CStr, CString}, ptr};
+use std::{
+    ffi::{c_char, CStr, CString},
+    ptr,
+};
 
 fn base64_url_decode(input: &str) -> Vec<u8> {
     decode_config(input, URL_SAFE_NO_PAD).unwrap()
@@ -55,7 +58,10 @@ fn check_jwt_property(jwt: &str, property: &str, exp_value: &str) -> bool {
     // Check in header
     if let Some(obj) = header.as_object() {
         if obj.contains_key(property) {
-            let value = obj.get(property).and_then(|value| value.as_str().map(|s| s.to_string())).unwrap();
+            let value = obj
+                .get(property)
+                .and_then(|value| value.as_str().map(|s| s.to_string()))
+                .unwrap();
             return value == exp_value;
         }
     }
@@ -63,7 +69,10 @@ fn check_jwt_property(jwt: &str, property: &str, exp_value: &str) -> bool {
     // Check in body
     if let Some(obj) = payload.as_object() {
         if obj.contains_key(property) {
-            let value = obj.get(property).and_then(|value| value.as_str().map(|s| s.to_string())).unwrap();
+            let value = obj
+                .get(property)
+                .and_then(|value| value.as_str().map(|s| s.to_string()))
+                .unwrap();
             return value == exp_value;
         }
     }
@@ -71,8 +80,9 @@ fn check_jwt_property(jwt: &str, property: &str, exp_value: &str) -> bool {
     false
 }
 
+#[allow(clippy::missing_safety_doc)]
 #[no_mangle]
-pub extern "C" fn get_property(jwt_cstr: *const c_char, prop_cstr: *const c_char) -> *mut c_char {
+pub unsafe extern "C" fn get_property(jwt_cstr: *const c_char, prop_cstr: *const c_char) -> *mut c_char {
     if jwt_cstr.is_null() || prop_cstr.is_null() {
         return ptr::null_mut();
     }
@@ -114,8 +124,9 @@ pub extern "C" fn get_property(jwt_cstr: *const c_char, prop_cstr: *const c_char
 }
 
 /// Free a C string returned from `get_property`
+#[allow(clippy::missing_safety_doc)]
 #[no_mangle]
-pub extern "C" fn free_string(s: *mut c_char) {
+pub unsafe extern "C" fn free_string(s: *mut c_char) {
     if !s.is_null() {
         unsafe {
             drop(CString::from_raw(s));
@@ -123,12 +134,13 @@ pub extern "C" fn free_string(s: *mut c_char) {
     }
 }
 
+#[allow(clippy::missing_safety_doc)]
 #[no_mangle]
-pub extern "C" fn verify_jwt(jwt_cstr: *const c_char) -> bool {
+pub unsafe extern "C" fn verify_jwt(jwt_cstr: *const c_char) -> bool {
     let x5c_certs = [
-    // This is the certificate of the attestation service, which can be
-    // found in tless/attestation-service/certs/cert.pem
-    r#"-----BEGIN CERTIFICATE-----
+        // This is the certificate of the attestation service, which can be
+        // found in tless/attestation-service/certs/cert.pem
+        r#"-----BEGIN CERTIFICATE-----
 MIIFCTCCAvGgAwIBAgIUIfCvnY9eL7gCAMnilTlwJTjV1ekwDQYJKoZIhvcNAQELBQAwFDESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTI1MDQxMzA5MzM1MFoXDTI2MDQxMzA5MzM1MFowFDESMBAGA1UEAwwJbG9jYWxob3N0MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAxiqavAStTeJz0b2fEbIOzzJBBdxKlZhkixFd1IbHbxCwp+pAkPSoMuNr4zhbQNMOCqTWx0yIsKA2rJw2DohFtQWQSIUor8OLyMV/I2XIJydR9pcW/ZLx4LcSbv5Q9PiJXk1VB+IjYoW/2b2BHc9lCEZB+RLVDCVXGex1Wi3IeGcNhTDJHquwIojo+1HGtEH/a3K9wgRdy1D0PmDQCNCxQoBajATA0u4/TpsVsjsZzB7ZJpI020m7BCMvi7Dy68kDq18CZpAW0ZT7YsvCY1X+D0BvXd0NVNg/udqMPeQvhSXkQsiPqWar3zsR8JC5oKGVei6bHhtX17/9PiOChyIDzWwcrVNtJnmdS4jzuFdNOaBlCFGseXf3Pxkee3N/9vF3mn6RPYJfgj7yjr9qmxnRj02L8wbw3E8YjhOkznLiARDVCivzggEaHRNgDv7p3bQACkYae2gzJh+roBSm7fVmUH46Rgk8rz54uh/kKqoGpyxFV9njVZ8Q5JO+LI2aUjAxE13mZqkd89DYuvgHp7K5UDw/Bi5S2CWb/mLTX/WKur53t+B7iE3kJFx0A8G2UxLg3q9yhH+n2p64suLMq9iZcIlU+pSQj3jMSpuJH/6IHRHvJojgnx1T0bPxFtevIkXCNCXdgAHXmr+J5M60au2xIODk974QMfin8rGhwKdkpP0CAwEAAaNTMFEwHQYDVR0OBBYEFOc0rW9L90ySukKVg879piXRzDU0MB8GA1UdIwQYMBaAFOc0rW9L90ySukKVg879piXRzDU0MA8GA1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggIBAD5pxBGpsYvEfhppvVfMakn9DaEKmDp2GGs5SElJY5QS89dWjV4h4GGSVHPlPJ3TIdM9Qkvr34JMsLvkBNrAhlmPMQJAPnjqo6kuLoDCk1PNTQPZA9rO9ljoTMNcTCZue3Hu5G96PwV9z3kzGZZaBndmEnBVQ5JLXxZ/2221kyPxeV5sKoSfR2ZhfQcZiaiudY89kdJSg+2KovUoRzxoWvkZZyRz2UZX/VGF8luUbw6UFZf/SlV+JK7bcD5kNuMrFVZdm8hLu07wrRuRVSmM9wbZtdpjcRNtledNd1a7Nd9k1Oqqn/JZO3DfzoPzclje26mNh2ASNhqmO1SifoBgJDMU7ZmO4KS/Euqb2hgzQbjOG1FRflz1XJ5yKjY1T/4YwBqw8zUVVtmMUj0ksNWvYByh1+ZWZZNm03ioWkER4z+9MwTbPUVPwtg+HwnJXoV8C6Er16/blCuS1xgYMrBB5mK86MXFgNdJ3xrdvuukDhE7Eil9iC5419giya4Rli81VUdSvdzd6bldXAKQqCf0jB3kjTx0lno5CtgTG1s23Gnm/mitSWbnoy5TGjgX8wsIFdYmGhljouan7kOKiOkSgfnsbhd/aqCwt5NuU5WQMSfQ50BsIkT0HftXqaagNqXGUgQ8vrUa4wo8vlgGv5fwS6kzPDJW45w0uwIS1uEbHN1T
 -----END CERTIFICATE-----"#,
     ];
@@ -138,8 +150,13 @@ MIIFCTCCAvGgAwIBAgIUIfCvnY9eL7gCAMnilTlwJTjV1ekwDQYJKoZIhvcNAQELBQAwFDESMBAGA1UE
     verify_jwt_signature(jwt, &x5c_certs)
 }
 
+#[allow(clippy::missing_safety_doc)]
 #[no_mangle]
-pub extern "C" fn check_property(jwt_cstr: *const c_char, property_cstr: *const c_char, exp_value_cstr: *const c_char) -> bool{
+pub unsafe extern "C" fn check_property(
+    jwt_cstr: *const c_char,
+    property_cstr: *const c_char,
+    exp_value_cstr: *const c_char,
+) -> bool {
     let jwt = unsafe { CStr::from_ptr(jwt_cstr).to_str().unwrap() };
     let property = unsafe { CStr::from_ptr(property_cstr).to_str().unwrap() };
     let exp_value = unsafe { CStr::from_ptr(exp_value_cstr).to_str().unwrap() };
