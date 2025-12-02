b5aa0a0c5ecd26a70a57816fc86325b04bcd5e22
Carlos Segarra <carlos@carlossegarra.com>
2025-12-02 18:10:46 +0000

    [scripts] E: Use TPM_GID In Entrypoint

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -4,5 +4,7 @@ sudo apt install -y \
     build-essential \
     clang-format \
     libfontconfig1-dev \
+    libtss2-dev \
+    libtss2-sys1t64 \
     libssl-dev \
     pkg-config > /dev/null 2>&1
