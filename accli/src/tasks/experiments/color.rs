5269ae9b897b8434179d1d2ef780275477b106e1
Carlos Segarra <carlos@carlossegarra.com>
2026-02-27 13:36:58 +0000

    [experiments] E: CSG Thesis Plots

--- accli/src/tasks/experiments/color.rs
+++ accli/src/tasks/experiments/color.rs
@@ -7,11 +7,16 @@ pub static STROKE_WIDTH: u32 = 5;
 
 pub fn get_color_from_label(label: &str) -> Result<RGBColor> {
     match label {
-        "dark-red" | "accless" => Ok(RGBColor(130, 1, 1)),
-        "dark-blue" | "accless-maa" => Ok(RGBColor(1, 6, 130)),
-        "dark-green" => Ok(RGBColor(0, 97, 29)),
-        "dark-orange" => Ok(RGBColor(163, 99, 2)),
-        "dark-yellow" => Ok(RGBColor(179, 176, 0)),
+        // "dark-red" | "accless" => Ok(RGBColor(130, 1, 1)),
+        "dark-red" | "accless" => Ok(RGBColor(213, 94, 0)),
+        // "dark-blue" | "accless-maa" => Ok(RGBColor(1, 6, 130)),
+        "dark-blue" | "accless-maa" => Ok(RGBColor(0, 114, 178)),
+        // "dark-green" => Ok(RGBColor(0, 97, 29)),
+        "dark-green" => Ok(RGBColor(0, 158, 115)),
+        // "dark-orange" => Ok(RGBColor(163, 99, 2)),
+        "dark-orange" => Ok(RGBColor(230, 159, 0)),
+        // "dark-yellow" => Ok(RGBColor(179, 176, 0)),
+        "dark-yellow" => Ok(RGBColor(240, 228, 66)),
         _ => {
             error!("unrecognized label for color (label={label})");
             anyhow::bail!("unrecognized label (label={label})");
